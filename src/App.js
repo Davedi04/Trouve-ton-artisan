6cfeca51fcd8395a94259b45c5c98a4f8c371ea2
Davedi04 <desmurgerdavid@gmail.com>
2025-01-24 14:43:18 +0100

    1er dépot

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import ArtisanList from './pages/ArtisanList';
+import ArtisanDetails from './pages/ArtisanDetails';
+import NotFound from './pages/NotFound';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+
+function App() {
+  return (
+    <Router>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/artisans" element={<ArtisanList />} />
+          <Route path="/artisans/:id" element={<ArtisanDetails />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div class='container-fluid'>
+            <h1>Header</h1>
+        </div>
+    );
+};
+
+export default Header;
